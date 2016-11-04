866ae0888c35de248cd9c55717f78b7dbd70d052
Erick Zuñiga <uhinfo22@gmail.com>
2016-11-04 15:12:02 -0600

    2

---
+++ Sistema_Ventas/DataAcesss/Objetos/Perfil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcesss.Objetos
+{
+    class Perfil
+    {
+        public int ID_Perfil { get; set; }
+        public string Tipo_Perfil { get; set; }
+    }
+}
---
+++ Sistema_Ventas/DataAcesss/Objetos/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAcesss.Objetos
+{
+    class Usuario
+    {
+        public int Cedula { get; set; }
+        public string Nombre_Usuario { get; set; }
+        public string Contraseña { get; set; }
+
+        Perfil Perfil = new Perfil();
+        public int Estado { get; set; }
+
+    }
+}
